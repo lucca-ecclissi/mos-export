eca9fe2017f1ec76cff3a5ae37285c717c0845f6
Lucca Ecclissi <lucca@lumminy.com>
2018-01-04 10:19:02 -0200

    Tried with callback

--- mjs_fs/api_lmos.js
+++ mjs_fs/api_lmos.js
@@ -1,15 +1,3 @@
 let LMOS = {
-    getSysInfo: ffi("void get_sys_info(void(*)(char*,userdata),userdata)"),
-    _rb: ffi("void reboot(int)"),
-    // ## **`getSysInfo(callback)`**
-    // Get system info 
-    // Returned json if success or null if C function not exist.
-    // getSysInfo: function (callback) {
-    //     this._gsi(callback)
-    // },
-    // ## **`reboot(delay)`**
-    // System reboot after delay miliseconds, default delay: 100
-    reboot: function (delay) {
-        this._rb(delay);
-    },
+    getSysInfo: ffi("void get_sys_info(void(*)(char*,userdata),userdata)")
 };
